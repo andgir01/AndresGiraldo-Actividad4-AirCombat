6be37a52b7b75e29f7328fdc72d4651dade27348
Andres Giraldo <andresgir0390@gmail.com>
2018-04-13 10:05:15 -0500

    Actividad 4

---
+++ AndresGiraldo-Actividad4/AirCombat/Assets/Scripts/Missile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//mover hacia adelante
+		this.transform.Translate (0.0f, 0.0f, 8.0f);
+
+	}
+
+	void OnCollisionEnter(Collision collision) { 
+
+
+		//Eliminamos el piso
+		Destroy (collision.collider.gameObject);
+	}
+		
+
+}
